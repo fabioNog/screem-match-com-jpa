43319032b7fad0726da7360e04a0304d41c8c7cc
fabionc <fabionc@unicamp.br>
2025-01-27 15:20:10 -0300

    Create Serie from serelize format

---
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+
+    Categoria(String categoriaOMDB){
+        this.categoriaOmdb = categoriaOMDB;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+}
