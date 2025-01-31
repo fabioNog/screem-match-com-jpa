34c0a5bc89cb64bc60cddd637555ece75542efdc
fabionc <fabionc@unicamp.br>
2025-01-31 09:57:32 -0300

    @Query(SELECT  inner join with episodios

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -1,8 +1,11 @@
 package br.com.alura.screenmatch.repository;
-
+import org.springframework.data.jpa.repository.Query;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository<Serie,Long> {
-
+    @Query("SELECT s FROM Serie s LEFT JOIN FETCH s.episodios WHERE s.id = :id")
+    Optional<Serie> findByIdWithEpisodes(@Param("id") Long id);
 }
